d6529526a04666b06256808798e425bed13648bc
pawelberowski <pawelberowski@gmail.com>
2023-11-24 15:11:03 +0100

    feat(Comments): made CRUD for comments

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { JwtAuthenticationGuard } from '../authentication/jwt-authentication.guard';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
 export class CommentsController {
@@ -11,5 +22,30 @@ export class CommentsController {
   @UseGuards(JwtAuthenticationGuard)
   create(@Body() comment: CreateCommentDto) {
     return this.commentsService.create(comment);
+  }
+
+  @Get()
+  getAll() {
+    return this.commentsService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.commentsService.getById(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthenticationGuard)
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() comment: UpdateCommentDto,
+  ) {
+    return this.commentsService.update(id, comment);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthenticationGuard)
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    await this.commentsService.delete(id);
   }
 }
