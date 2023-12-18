7f6ac1b2f8542408d033dc6d2f6e4b61bc6bba99
pawelberowski <pawelberowski@gmail.com>
2023-12-18 18:04:45 +0100

    refactor(categories-merge): added separate service for categories-merge

--- src/categories/categories-merge.controller.ts
+++ src/categories/categories-merge.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Patch, UseGuards } from '@nestjs/common';
-import { CategoriesService } from './categories.service';
 import { JwtAuthenticationGuard } from '../authentication/jwt-authentication.guard';
+import { CategoriesMergeService } from './categories-merge.service';
 
 @Controller('categories-merge')
 export class CategoriesMergeController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(
+    private readonly categoriesMergeService: CategoriesMergeService,
+  ) {}
 
   @Patch()
   @UseGuards(JwtAuthenticationGuard)
   mergeCategories() {
-    return this.categoriesService.mergeDuplicateCategories();
+    return this.categoriesMergeService.mergeDuplicateCategories();
   }
 }
--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -3,10 +3,11 @@ import { CategoriesService } from './categories.service';
 import CategoriesController from './categories.controller';
 import { DatabaseModule } from '../database/database.module';
 import { CategoriesMergeController } from './categories-merge.controller';
+import { CategoriesMergeService } from './categories-merge.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [CategoriesController, CategoriesMergeController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, CategoriesMergeService],
 })
 export class CategoriesModule {}
